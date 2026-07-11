8901fc03d309b5b598c5a06ad7feb606285c8c98
munnaMia <mdmunnamia987@gmail.com>
2026-07-11 15:55:04 +0600

    Add configuration loading functionality with dotenv support

--- config/config.go
+++ config/config.go
@@ -1,1 +1,33 @@
 package config
+
+import "github.com/joho/godotenv"
+
+var configurations *Configuration
+
+type Configuration struct{
+	Service Service
+}
+
+type Service struct {
+	Name      string
+	Version   string
+	HTTP_Port int
+}
+
+func loadConfiguration() {
+	err := godotenv.Load()
+	if err != nil {
+		// msg and os exit 1
+	}
+
+	// load the envs.
+	// write the envs value on configurations variable
+}
+
+func GetConfiguration() *Configuration {
+	if configurations == nil {
+		loadConfiguration()
+	}
+
+	return configurations
+}
