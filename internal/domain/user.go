d0edeb86d1b14425cbe0304b1dd2acebd822a455
munnaMia <mdmunnamia987@gmail.com>
2026-07-29 16:35:49 +0600

    Refactor User struct and UserRepository interface for improved clarity and organization

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -1,7 +1,20 @@
 package domain
 
+import (
+	"context"
+	"time"
+)
+
 type User struct {
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Id        int       `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+// UserRepository is the PORT defined by the domain
+type UserRepository interface {
+	Create(ctx context.Context, u *User) (*User, error)
 }
